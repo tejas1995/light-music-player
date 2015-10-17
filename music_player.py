ca4cd3cac4b1e0857da64e6419f6e2904863c72d
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2015-10-17 09:23:08 +0530

    Moved musicPlayer class to new module

---
+++ music_player.py
@@ -0,0 +1,29 @@
+import pyglet
+
+class musicPlayer(pyglet.media.Player):
+
+    def __init__(self):
+        pyglet.media.Player.__init__(self)
+        self.dequeued_sources = []
+        self.list_sources = []
+
+    def add_to_queue(self, source):
+        # Add song to playlist queue, and to list of sources
+        self.queue(source)
+        self.list_sources.append(source)
+
+    def next_song(self):
+        # Play the next song, set timestamp to 0 to play from beginning
+        self.next_source()
+        self._groups[-1].seek(0)
+
+    def previous_song(self):
+        # Find and play the previous song, set timestamp to 0 to play from beginning
+        current_source = self._groups[-1]._sources[0]
+        prev_source = self.list_sources[ self.list_sources.index(current_source)-1 ]
+        self._groups[-1]._timestamp_offset -= prev_source.duration
+        self._groups[-1]._dequeued_durations = self._groups[-1]._dequeued_durations[1:]
+        self._groups[-1]._sources.insert(0, prev_source)
+        self._groups[-1].duration += prev_source.duration
+        self._groups[-1].seek(0)
+
